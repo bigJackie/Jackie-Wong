765801ce74523f71f6682ccc84c4e2abff0a90b7
Jackie <2537020964@qq.com>
2023-03-02 14:39:13 +0800

    chore: init

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,23 +12,20 @@ const modules = import.meta.glob("../../posts/**/*.md");
 for (let i in modules) {
   let item = modules[i];
   const post_path = item.name.replace(/(.*[post]\/)(.*)\..*/gi, "$2");
-  console.log(posts);
-
-  posts.push({
-    path: "/blog/" + post_path,
-    name: post_path,
-    component: modules[item.name]().then(res => {
-      blogs.value.push({
-        title: (res as Post).title,
-        category: (res as Post).category,
-        date: (res as Post).date,
-        desc: (res as Post).desc,
-        path: post_path,
-      });
-      return res;
-    }),
-  });
-  console.log(posts);
+  modules[item.name]().then(res => {
+    blogs.value.push({
+      title: (res as Post).title,
+      category: (res as Post).category,
+      date: (res as Post).date,
+      desc: (res as Post).desc,
+      path: post_path,
+    });
+  }),
+    posts.push({
+      path: "/blog/" + post_path,
+      name: post_path,
+      component: modules[item.name],
+    });
 }
 
 const router = createRouter({
